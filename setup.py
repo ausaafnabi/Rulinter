9e6695a0f81fe4f10a13ddaa2196a1fb3b989d31
ausaafnabi <ausaafnabi@gitlab.com>
2024-12-12 12:54:13 +0300

    flake8 linter for russian comments RU001

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+requires = ["flake8>3.0.0"]
+setup(
+    name='flake8-russian-comments',
+    version='1.0',
+    description='A Flake8 plugin to restrict Russian comments',
+    author='AusaafNabi',
+    author_email='no-reply@example.com',
+    url='https://github.com/ausaafnabi/Rulinter',
+    py_modules=['_russian_comments_checker'],
+    install_requires=requires,
+    entry_points={
+        'flake8.extension': [
+            'RU001 = _russian_comments_checker:RussianCommentsChecker',
+        ],
+    },
+)
+
---
+++ test/test-comments.py
@@ -0,0 +1,11 @@
+# глобальный комментарий
+def test_comments():
+    #Стандартный тестовый комментарий внутри функции
+    return 0
+ 
+def test_multiline():
+    '''
+    Some english comment,это российский контекст
+    '''
+    
+    return 'тест'
